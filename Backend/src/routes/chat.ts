867eecf7cd6835b6708bc9835ddf0285dbf9894e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-08-22 12:12:08 +0530

    db and utils compltete

--- Backend/src/routes/chat.ts
+++ Backend/src/routes/chat.ts
@@ -19,4 +19,53 @@ router.post("/test", async (req, res) => {
   }
 });
 
+// get all threads
+
+router.get("/thread", async (req, res) => {
+  try {
+    const threads = await Thread.find({}).sort({ updatedAt: -1 });
+    // most recent thread on top: decending order on UpdatedAt
+    res.json(threads);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "failed ot fetch threads" });
+  }
+});
+
+// get one thread
+
+router.get("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const thread = await Thread.findById(threadId);
+
+    if (!thread) {
+      return res.status(404).json({ error: "Thread not found" });
+    }
+
+    res.json(thread.messages);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "failed ot fetch thread" });
+  }
+});
+
+// delete a thread
+
+router.delete("/thread/:threadId", async (req, res) => {
+  const { threadId } = req.params;
+  try {
+    const deletedThread = await Thread.findOneAndDelete({ threadId });
+
+    if (!deletedThread) {
+      res.status(404).json({ error: "thread not found" });
+    }
+
+    res.status(200).json({ success: "thread deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "failed ot fetch thread" });
+  }
+});
+
 export default router;
